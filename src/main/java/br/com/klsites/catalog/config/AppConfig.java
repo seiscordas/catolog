e6d95c58630d27709bddc4849ae1d67f456d839c
Kleber Martins <kleber@klsites.com.br>
2022-12-27 16:51:32 -0300

    up

--- src/main/java/br/com/klsites/catalog/config/AppConfig.java
+++ src/main/java/br/com/klsites/catalog/config/AppConfig.java
@@ -1,5 +1,6 @@
 package br.com.klsites.catalog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,6 +9,8 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
+    @Value("${jwt.secret}")
+    String jwtSecret;
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -15,7 +18,7 @@ public class AppConfig {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("MY-JWT-SECRET");
+        tokenConverter.setSigningKey(jwtSecret);
         return tokenConverter;
     }
     @Bean
